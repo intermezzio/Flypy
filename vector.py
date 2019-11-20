2538e3b48c59b2c0815bbe344df7a6e66205f381
Andrew Mascillaro <amascillaro@gmail.com>
2019-11-20 11:04:42 -0500

    Create vector methods for addition, multiplication, and dot products

---
+++ vector.py
@@ -0,0 +1,26 @@
+
+
+class Vector:
+
+    def __init___(self,x=0,y=0,z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def self.mag(self):
+        return (self.x**2 + self.y**2 + self.z**2) ** 1/2
+
+    def __add__(self, v2):
+        return Vector(self.x + v2.x, self.y + v2.y, self.z + v2.z)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def dotP(self, v2):
+        self.x *= v2.x
+        self.y *= v2.y
+        self.z *= v2.z
+        return
+
+    def crossP(self, v2):
+        pass
