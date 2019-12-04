a146306cc80d51bc6dd7af0c3e426caecf3ede6b
Andrew Mascillaro <amascillaro@gmail.com>
2019-12-04 11:56:07 -0500

    Move the main drone method to a callable function

--- main.py
+++ main.py
@@ -8,21 +8,6 @@ import sys
 
 from drone import Drone
 
-
-params = [
-    "x",
-    "y",
-    "z",
-    "v_x",
-    "v_y",
-    "v_z",
-    "roll",
-    "pitch",
-    "yaw"
-]
-
-# data = pd.DataFrame(cols=params)
-
 def goodRotorSpeeds(avg_speed, rollpitch=0, rollpitch_power=0, yaw_power=0):
     """
     Generate rotor speeds given a desired direction
@@ -80,7 +65,6 @@ def runDrone(drone, dx = 0, dy = 0, dz = 0):
     del drone_props
     return (x, y, z, t, dist)
 
-
 def angleToCardinal(angle):
     """
     Given an angle (in radians), return the cardinal direction
@@ -98,66 +82,83 @@ def angleToCardinal(angle):
 # Assign target drone coordinates
 target_xyz = (3,3,3) if len(sys.argv) < 4 else [int(i) for i in sys.argv[1:4]]
 
-# Solutions is a nested dictionary
-solutions = dict()
-min_dist = np.inf
+def moveDrone(*target_xyz):
+    """
+    Given a target x, y, z find a rotor setting that gets the drone there
+    Outputs results to a JSON
+    
+    Parameters:
+        target_xyz: x, y, and z values (unpacked)
 
-# Min_props is a dicitonary for optimal settings
-min_props = dict()
+    Returns:
+        Dict: nested dict with all tries and the optimal rotor setting
 
-# Sweep through drone attributes
-for hover_spd in range(580, 740, 20):
-    # Create nested dict
-    solutions[hover_spd] = dict()
-    print("hover_spd:", hover_spd)
+    """
 
-    for angle in np.arange(0,2*pi, pi/4): # sweep 45 degrees at a time around in a circle
+    # Solutions is a nested dictionary
+    solutions = dict()
+    min_dist = np.inf
 
-        c_angle = angleToCardinal(angle)
-        solutions[hover_spd][c_angle] = dict()
+    # Min_props is a dicitonary for optimal settings
+    min_props = dict()
 
-        for rollpitch_power in (1, 5): # choose between a lot and a little roll/pitch
-            solutions[hover_spd][c_angle][rollpitch_power] = dict()
+    # Sweep through drone attributes
+    for hover_spd in range(580, 740, 20):
+        # Create nested dict
+        solutions[hover_spd] = dict()
+        print("hover_spd:", hover_spd)
 
-            for yaw_power in (-1, 0, 1): # choose yaw and direction
-                thisCase = dict()
+        for angle in np.arange(0,2*pi, pi/4): # sweep 45 degrees at a time around in a circle
 
-                # Generate rotor speeds from properties
-                r_speed = goodRotorSpeeds(hover_spd, rollpitch=angle, 
-                          rollpitch_power=rollpitch_power, yaw_power=yaw_power)
-                
-                # Create drone and model it
-                drone = Drone(r_speed=r_speed)
-                x, y, z, t, dist = runDrone(drone, *target_xyz)
-                
-                # Store values
-                thisCase["x"], thisCase["y"], thisCase["z"] = x, y, z
-                thisCase["t"], thisCase["dist"] = t, dist
-                
-                # if optimal solution
-                if thisCase["dist"] < min_dist:
-                    min_props = {
-                        "hover_spd": hover_spd,
-                        "c_angle": c_angle,
-                        "angle": angle,
-                        "rollpitch_power": rollpitch_power,
-                        "yaw_power": yaw_power,
-                        "min_dist": thisCase["dist"],
-                        "x": thisCase["x"],
-                        "y": thisCase["y"],
-                        "z": thisCase["z"]
-                    }
-                    min_dist = thisCase["dist"]
-                solutions[hover_spd][c_angle][rollpitch_power][yaw_power] = thisCase
+            c_angle = angleToCardinal(angle)
+            solutions[hover_spd][c_angle] = dict()
 
-# Create full dictionary
-full_dict = {
-    "tries": solutions,
-    "key": min_props
-}
+            for rollpitch_power in (1, 5): # choose between a lot and a little roll/pitch
+                solutions[hover_spd][c_angle][rollpitch_power] = dict()
 
-# Write to JSON
-with open(f"x{target_xyz[0]}y{target_xyz[1]}z{target_xyz[2]}.json", "w") as js:
-    json.dump(full_dict, js, indent=4, sort_keys=True)
+                for yaw_power in (-1, 0, 1): # choose yaw and direction
+                    thisCase = dict()
 
-print(min_props)
+                    # Generate rotor speeds from properties
+                    r_speed = goodRotorSpeeds(hover_spd, rollpitch=angle, 
+                              rollpitch_power=rollpitch_power, yaw_power=yaw_power)
+                    
+                    # Create drone and model it
+                    drone = Drone(r_speed=r_speed)
+                    x, y, z, t, dist = runDrone(drone, *target_xyz)
+                    
+                    # Store values
+                    thisCase["x"], thisCase["y"], thisCase["z"] = x, y, z
+                    thisCase["t"], thisCase["dist"] = t, dist
+                    
+                    # if optimal solution
+                    if thisCase["dist"] < min_dist:
+                        min_props = {
+                            "hover_spd": hover_spd,
+                            "c_angle": c_angle,
+                            "angle": angle,
+                            "rollpitch_power": rollpitch_power,
+                            "yaw_power": yaw_power,
+                            "min_dist": thisCase["dist"],
+                            "x": thisCase["x"],
+                            "y": thisCase["y"],
+                            "z": thisCase["z"]
+                        }
+                        min_dist = thisCase["dist"]
+                    solutions[hover_spd][c_angle][rollpitch_power][yaw_power] = thisCase
+
+    # Create full dictionary
+    full_dict = {
+        "tries": solutions,
+        "key": min_props
+    }
+
+    # Write to JSON
+    with open(f"x{target_xyz[0]}y{target_xyz[1]}z{target_xyz[2]}.json", "w") as js:
+        json.dump(full_dict, js, indent=4, sort_keys=True)
+
+    return full_dict
+
+
+if __name__ == "__main__":
+    print(moveDrone(*target_xyz)["key"])
